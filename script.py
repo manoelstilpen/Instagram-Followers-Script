3b3854bf158b3b4f621f6ff39c551aad05209a7a
Manoel Stilpen <manoelstilpen@github.com>
2017-04-07 00:42:56 -0300

    funcionando, porem, e necessario ajustes

--- script.py
+++ script.py
@@ -1,20 +1,76 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://instagram.com")
-assert "Instagram" in driver.title
+chrome = webdriver.Firefox()
+chrome.get("http://instagram.com")
+assert "Instagram" in chrome.title
 
-elem = driver.find_element_by_class_name("_fcn8k")
-elem.click()
-elem = driver.find_element_by_name("username")
-elem.send_keys("manoelstilpen")
-elem = driver.find_element_by_name("password")
-elem.send_keys("")
-elem.send_keys(Keys.RETURN)
+sleep(2)
 
-<a class="_soakw _vbtk2 coreSpriteDesktopNavProfile" href="/manoelstilpen/">Perfil</a>
-# //*[@id="react-root"]/section/nav/div/div/div/div[3]/div/div[3]/a
-# elem = driver.find_element_by_xpath("//section/nav/div/div/div/div[3]/div/div[3]/a")
-elem = driver.find_element_by_tag_name("")
-elem.click()
+logar = chrome.find_element_by_xpath("//a[@class='_fcn8k']")
+logar.click()
+
+name = chrome.find_element_by_name("username")
+passw = chrome.find_element_by_name("password")
+
+name.send_keys("manoelstilpen")
+passw.send_keys("")
+
+passw.send_keys(Keys.RETURN)
+
+sleep(3) # wait to load the page
+
+perfil = chrome.find_element_by_xpath("//a[contains(@class,'coreSpriteDesktopNavProfile')]")
+perfil.click()
+
+sleep(2)
+
+# abre popup dos seguidores
+followers_icon = chrome.find_element_by_xpath("//a[contains(@href,'/manoelstilpen/followers/')]")
+followers_icon.click()
+
+sleep(2)
+
+popup_followers = chrome.find_element_by_xpath("//div[contains(@class,'_4gt3b')]")
+popup_followers.click()
+
+#carrega todos os seguidores
+for _ in range(0, 30):
+    popup_followers.send_keys(Keys.PAGE_DOWN)
+    popup_followers.send_keys(Keys.PAGE_DOWN)
+    sleep(1)
+
+elements_followers = chrome.find_elements_by_xpath("//a[contains(@class,'_4zhc5')]")
+list_followers = map(lambda x: x.get_attribute('innerHTML'), elements_followers)
+
+#for follower in list_followers:
+#   print follower.get_attribute('innerHTML')
+
+print len(list_followers)
+
+bt_close = chrome.find_element_by_xpath("//div[@class='_quk42']")
+bt_close.send_keys(Keys.ESCAPE)
+
+following_icon = chrome.find_element_by_xpath("//a[contains(@href,'/manoelstilpen/following/')]")
+following_icon.click()
+
+sleep(2)
+
+popup_following = chrome.find_element_by_xpath("//div[contains(@class,'_4gt3b')]")
+popup_following.click()
+
+# carrega todos os seguintes
+for _ in range(0, 30):
+    popup_following.send_keys(Keys.PAGE_DOWN)
+    popup_following.send_keys(Keys.PAGE_DOWN)
+    sleep(1)
+
+elements_following = chrome.find_elements_by_xpath("//a[contains(@class,'_4zhc5')]")
+list_following = map(lambda x : x.get_attribute('innerHTML'), elements_following)
+
+for following in list_following:
+    if following not in list_followers:
+        print following
+
+print len(list_following)
