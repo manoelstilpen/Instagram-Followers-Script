9bcecefa66e0319e9dac693693d676089a5db73d
Manoel Stilpen <manoelstilpen@github.com>
2017-04-07 01:29:15 -0300

    alterado modo de execucao

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
+import sys
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+username = sys.argv[1]
+password = sys.argv[2]
 
 chrome = webdriver.Firefox()
 chrome.get("http://instagram.com")
@@ -14,8 +18,8 @@ logar.click()
 name = chrome.find_element_by_name("username")
 passw = chrome.find_element_by_name("password")
 
-name.send_keys("manoelstilpen")
-passw.send_keys("")
+name.send_keys(username)
+passw.send_keys(password)
 
 passw.send_keys(Keys.RETURN)
 
@@ -27,7 +31,7 @@ perfil.click()
 sleep(2)
 
 # abre popup dos seguidores
-followers_icon = chrome.find_element_by_xpath("//a[contains(@href,'/manoelstilpen/followers/')]")
+followers_icon = chrome.find_element_by_xpath("//a[contains(@href,'/"+username+"/followers/')]")
 followers_icon.click()
 
 sleep(2)
@@ -35,7 +39,7 @@ sleep(2)
 popup_followers = chrome.find_element_by_xpath("//div[contains(@class,'_4gt3b')]")
 popup_followers.click()
 
-#carrega todos os seguidores
+#carrega todos os que me seguem
 for _ in range(0, 30):
     popup_followers.send_keys(Keys.PAGE_DOWN)
     popup_followers.send_keys(Keys.PAGE_DOWN)
@@ -44,15 +48,12 @@ for _ in range(0, 30):
 elements_followers = chrome.find_elements_by_xpath("//a[contains(@class,'_4zhc5')]")
 list_followers = map(lambda x: x.get_attribute('innerHTML'), elements_followers)
 
-#for follower in list_followers:
-#   print follower.get_attribute('innerHTML')
-
 print len(list_followers)
 
 bt_close = chrome.find_element_by_xpath("//div[@class='_quk42']")
 bt_close.send_keys(Keys.ESCAPE)
 
-following_icon = chrome.find_element_by_xpath("//a[contains(@href,'/manoelstilpen/following/')]")
+following_icon = chrome.find_element_by_xpath("//a[contains(@href,'/"+username+"/following/')]")
 following_icon.click()
 
 sleep(2)
@@ -60,14 +61,14 @@ sleep(2)
 popup_following = chrome.find_element_by_xpath("//div[contains(@class,'_4gt3b')]")
 popup_following.click()
 
-# carrega todos os seguintes
+# carrega todos os que sigo
 for _ in range(0, 30):
     popup_following.send_keys(Keys.PAGE_DOWN)
     popup_following.send_keys(Keys.PAGE_DOWN)
     sleep(1)
 
 elements_following = chrome.find_elements_by_xpath("//a[contains(@class,'_4zhc5')]")
-list_following = map(lambda x : x.get_attribute('innerHTML'), elements_following)
+list_following = map(lambda x: x.get_attribute('innerHTML'), elements_following)
 
 for following in list_following:
     if following not in list_followers:
